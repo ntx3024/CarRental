36342a782f726f23692e982c4084775676047154
KillianO92 <kobrien@springfieldcollege.edu>
2017-04-18 15:19:54 -0400

    Added to customer tab

--- MainWindow.py
+++ MainWindow.py
@@ -1,17 +1,20 @@
 from tkinter import *
 from tkinter import ttk
+import ipywidgets as ipyw
 from PIL import ImageTk, Image
 from MakesDBFunctions import *
 from CarsDBFunctions import *
+from CustDBFunctions import *
 from TVClass import TVClassExample
 
 
 #Not sure best way to call these
 dbMk = MakesDBFunctions()
 dbCars = CarsDBFunctions()
+dbCust = CustDBFunctions()
 
 class MainWindow:
-    def __init__(self, master):
+    def __init__(self, master): 
         #Create a Variable for Makes class
         self.master = master
         self.master.geometry("800x500")
@@ -35,7 +38,7 @@ class MainWindow:
         self.nb.add(self.tabCustomers, text="Customers")
         self.nb.add(self.tabReservations, text="Reservations")
         self.nb.add(self.tabTree, text="VehiclesTree")
-        self.nb.pack(side=BOTTOM, fill=BOTH, expand=Y)
+        self.nb.pack(side=BOTTOM, fill=BOTH, expand=1)
         
         self.lstMakes = Listbox(self.tabVehicles, selectmode=SINGLE, relief=FLAT)
         allMakes = dbMk.loadAllMakes()
@@ -46,7 +49,7 @@ class MainWindow:
             self.lstMakes.insert(END, m[1])
 
         self.lstMakes.bind('<<ListboxSelect>>', self.lstMakeSelectionChanged)
-        self.lstMakes.pack(side=LEFT, fill=BOTH)
+        self.lstMakes.pack(side=LEFT, fill=BOTH, expand=1)
 
         self.lstModels = Listbox(self.tabVehicles, selectmode=SINGLE, relief=FLAT)
         self.lstModels.insert(END, "Models")
@@ -54,16 +57,15 @@ class MainWindow:
         self.lstModels.insert(END, " ")
         
         self.lstModels.bind('<<ListboxSelect>>', self.lstModelSelectionChanged)
-        self.lstModels.pack(side=LEFT, fill=Y)
+        self.lstModels.pack(side=LEFT, fill=BOTH, expand=1)
 
         self.lstDetails = Listbox(self.tabVehicles, selectmode=SINGLE, relief=FLAT)
         self.lstDetails.insert(END, "Details")
         self.lstDetails.insert(END, "----------")
         self.lstDetails.insert(END, " ")
-        self.lstDetails.pack(side=LEFT, fill=Y)
+        self.lstDetails.pack(side=LEFT, fill=BOTH, expand=1)
 
-        Label(self.tabCustomers, text='First Name', relief=FLAT).grid(row = 1, column = 0)
-
+        
         models = dbCars.loadAllCars()
         self.treeCars = ttk.Treeview(self.tabTree)
         self.treeCars['show']='headings'
@@ -80,8 +82,65 @@ class MainWindow:
         self.treeCars.heading("Doors", text="Doors", anchor=E)
         for index, dat in enumerate(models):
             self.treeCars.insert("",index, values=(dat[0], dat[1], dat[2], dat[3]))
-        self.treeCars.pack(side=BOTTOM, fill=BOTH)
+        self.treeCars.pack(side=TOP, fill=BOTH, expand=1)
 
+        Label(self.tabCustomers, text='First Name', relief=FLAT).pack(side=LEFT, anchor=N+W)
+        self.fNameEntry = Entry(master=self.tabCustomers).pack(side=LEFT, anchor=N+W)
+
+        Label(self.tabCustomers, text='Last Name', relief=FLAT).pack(side=LEFT, anchor=N+W)
+        self.lNameEntry = Entry(master=self.tabCustomers).pack(side=LEFT, anchor=N+W)
+
+        Label(self.tabCustomers, text='Phone Number', relief=FLAT).pack(side=LEFT, anchor=N+W)
+        self.phoneNum = Entry(master=self.tabCustomers).pack(side=LEFT, anchor=N+W)
+        
+        self.addBtn = Button(self.tabCustomers, text='Add Customer')
+        self.addBtn.pack(side=LEFT, anchor=N+W)
+
+        self.histBtn = Button(self.tabCustomers, text='Show History')
+        self.histBtn.pack(side=LEFT, anchor=N+W)
+
+        custHist = dbCust.loadCustHist()
+        self.treeCust = ttk.Treeview(self.tabCustomers)
+        self.treeCust['show']='headings'
+        self.treeCust["columns"] = ("FirstName","LastName","ReservationID","Model", "StartDate","EndDate","RealStartDate","RealEndDate")
+        self.treeCust.column("FirstName", width=100, anchor=S)
+        self.treeCust.column("LastName", width=175, anchor=S)
+        self.treeCust.column("ReservationID", width=175, anchor=S)
+        self.treeCust.column("Model", width=100, anchor=S)
+        self.treeCust.column("StartDate", width=100, anchor=S)
+        self.treeCust.column("EndDate", width=175, anchor=S)
+        self.treeCust.column("RealStartDate", width=175, anchor=S)
+        self.treeCust.column("RealEndDate", width=100, anchor=S)
+        #tree.heading("#0", text='ID', anchor='w')
+        #tree.column("#0", anchor="w")
+        self.treeCust.heading("FirstName", text="First Name", anchor=W)
+        self.treeCust.heading("LastName", text="Last Name")
+        self.treeCust.heading("ReservationID", text="Reservation ID")
+        self.treeCust.heading("Model", text="Model")
+        self.treeCust.heading("StartDate", text="Start Date")
+        self.treeCust.heading("EndDate", text="End Date")
+        self.treeCust.heading("RealStartDate", text="Real Start Date")
+        self.treeCust.heading("RealEndDate", text="Real End Date", anchor=E)
+        for index, dat in enumerate(custHist):
+            self.treeCust.insert("",index, values=(dat[0], dat[1], dat[2], dat[3], dat[4], dat[5], dat[6]))
+        self.treeCust.pack(side=BOTTOM, fill=BOTH, expand=1)
+
+        
+        
+        
+        
+##        Label(self.tabReservations, text='First Name', relief=FLAT).grid(row = 1, column = 0)
+##        self.fNameEntry = Entry(master=self.tabReservations).grid(row=1, column = 1)
+##
+##        Label(self.tabReservations, text='Last Name', relief=FLAT).grid(row = 1, column = 2)
+##        self.lNameEntry = Entry(master=self.tabReservations).grid(row=1, column = 3)
+##
+##        Label(self.tabReservations, text='Pickup Day', relief=FLAT).grid(row=2, column=0)
+##        self.puEntry = Entry(master=self.tabReservations).grid(row=2, column=1)
+##        self.puEntry#(8, 'MM-DD-YYYY')
+##
+##        Label(self.tabReservations, text='Drop Off Day', relief=FLAT).grid(row=2, column=2)
+##        self.doEntry = Entry(master=self.tabReservations).grid(row=2, column=3)
 
     def greet(self):
         print("Greetings!")
@@ -154,7 +213,7 @@ class MainWindow:
             self.lstDetails.insert(END, "Colors:\n")
             for index, dat in enumerate(cars):
                 self.lstDetails.insert(END, "              {}".format(dat[2]))
-            self.lstDetails.insert(END, "Door: {}".format(dat[1]))
+            self.lstDetails.insert(END, "Doors: {}".format(dat[1]))
 
 
 
